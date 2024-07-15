ed5be6365e42670f3de4c2e4f1ce3193281206df
antonio99686 <antonio.2022324018@aluno.iffar.edu.br>
2024-07-15 18:31:04 -0300

    execução

---
+++ function.php
@@ -0,0 +1,30 @@
+<?php
+function conectar()
+{
+    require_once "config.php";
+    $conexao = mysqli_connect(
+        $config['host'],
+        $config['user'],
+        $config['pass'],
+        $config['db']
+    );
+
+    if ($conexao === false) {
+        echo "Erro ao conectar á base dados.N° do erro" .
+            mysqli_connect_errno() . "." .
+            mysqli_connect_error();
+        exit;
+    }
+    return $conexao;
+}
+
+
+function executarSQL($conexao, $sql)
+{
+    $result = mysqli_query($conexao, $sql);
+    if ($result === false) {
+        echo "Erro ao executar o comando SQL. " . mysqli_errno($conexao) . ": " . mysqli_error($conexao);
+        die(); // Stop further execution if there's an error
+    }
+    return $result;
+}
